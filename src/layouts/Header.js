eeed7fa9a5f71c413e648b139be8481879b3a13f
Ikhee <ikhbayar@fiba.mn>
2025-01-21 03:13:53 +0800

    gg

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -21,7 +21,7 @@ const Header = () => {
     const { t } = useTranslation()
 
     return (
-        <div as="nav" className="bg-primary-500">
+        <div className="bg-primary-500 sticky top-0 z-10">
             <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                 <div className="relative flex h-16 items-center justify-between">
                     <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
--- src/pages/Destinations.js
+++ src/pages/Destinations.js
@@ -16,7 +16,8 @@ const Destinations = () => {
         var chart = root.container.children.push(
             am5map.MapChart.new(root, {
                 panX: "rotateX",
-                projection: am5map.geoNaturalEarth1()
+                projection: am5map.geoMercator(),
+                homeGeoPoint: { latitude: 2, longitude: 2 }
             })
         );
 
@@ -34,33 +35,193 @@ const Destinations = () => {
         });
 
         polygonSeries.mapPolygons.template.states.create("hover", {
-            fill: am5.color(0x677935)
-        });
-
-        chart.events.on("click", function (ev) {
-            console.log(chart.invert(ev.point))
+            fill: am5.color('#9fbfde')
         });
 
         polygonSeries.mapPolygons.template.events.on("click", (ev) => {
             var dataItem = ev.target.dataItem;
             var data = dataItem.dataContext;
-            // var zoomAnimation = polygonSeries.zoomToDataItem(dataItem);
             console.log(data);
-            // Promise.all([
-            //     zoomAnimation.waitForStop(),
-            //     am5.net.load("https://cdn.amcharts.com/lib/5/geodata/json/" + data.map + ".json", chart)
-            // ]).then((results) => {
-            //     var geodata = am5.JSONParser.parse(results[1].response);
-            //     countrySeries.setAll({
-            //         geoJSON: geodata,
-            //         fill: data.polygonSettings.fill
-            //     });
-
-            //     countrySeries.show();
-            //     worldSeries.hide(100);
-            //     backContainer.show();
-            // });
         });
+
+        // this will be invisible line (note strokeOpacity = 0) along which invisible points will animate
+        let lineSeries = chart.series.push(am5map.MapLineSeries.new(root, {}));
+        lineSeries.mapLines.template.setAll({
+            stroke: root.interfaceColors.get("alternativeBackground"),
+            strokeOpacity: 0.3
+        });
+
+        // this will be visible line. Lines will connectg animating points so they will look like animated
+        let animatedLineSeries = chart.series.push(am5map.MapLineSeries.new(root, {}));
+        animatedLineSeries.mapLines.template.setAll({
+            stroke: root.interfaceColors.get("alternativeBackground"),
+            strokeOpacity: 0.6
+        });
+
+        // invisible series which will animate along invisible lines
+        let animatedBulletSeries = chart.series.push(
+            am5map.MapPointSeries.new(root, {})
+        );
+
+        animatedBulletSeries.bullets.push(function () {
+            let circle = am5.Circle.new(root, {
+                radius: 0
+            });
+
+            return am5.Bullet.new(root, {
+                sprite: circle
+            });
+        });
+
+
+        let cities = [
+            {
+                id: "mongolia",
+                title: "Ulaanbaatar",
+                geometry: { type: "Point", coordinates: [103.8467, 46.8625] },
+            },
+            {
+                id: "frankfurt",
+                title: "Frankfurt",
+                geometry: { type: "Point", coordinates: [8.6821, 50.1109] }
+            },
+            {
+                id: "hongkong",
+                title: "Hong Kong",
+                geometry: { type: "Point", coordinates: [114.2, 22.3] }
+            },
+        ];
+
+        // destination series
+        let citySeries = chart.series.push(
+            am5map.MapPointSeries.new(root, {})
+        );
+
+        let planeSeries = chart.series.push(am5map.MapPointSeries.new(root, {}));
+        let pointSeries = chart.series.push(am5map.MapPointSeries.new(root, {}));
+
+        let point1 = addCity({ latitude: 46.8625, longitude: 103.8467 }, "Ulaanbaatar");
+        let point2 = addCity({ latitude: 50.1109, longitude: 8.6821 }, "Frankfurt");
+
+        let lineDataItem = lineSeries.pushDataItem({
+            pointsToConnect: [point1, point2]
+        });
+
+
+        let plane = am5.Graphics.new(root, {
+            svgPath:
+                "m2,106h28l24,30h72l-44,-133h35l80,132h98c21,0 21,34 0,34l-98,0 -80,134h-35l43,-133h-71l-24,30h-28l15,-47",
+            scale: 0.06,
+            centerY: am5.p50,
+            centerX: am5.p50,
+            fill: am5.color('#012853')
+        });
+
+        planeSeries.bullets.push(function () {
+            let container = am5.Container.new(root, {});
+            container.children.push(plane);
+            return am5.Bullet.new(root, { sprite: container });
+        });
+
+        // visible city circles
+        citySeries.bullets.push(function () {
+            let circle = am5.Circle.new(root, {
+                radius: 5,
+                tooltipText: "{title}",
+                tooltipY: 0,
+                fill: am5.color(0xffba00),
+                stroke: root.interfaceColors.get("background"),
+                strokeWidth: 2
+            });
+
+            circle.events.on("click", function (event) {
+                planeDataItem.set("positionOnLine", 0);
+                // planeDataItem.clear();
+                resetPlaneAnimation()
+                var dataItem = event.target.dataItem;
+                var data = dataItem.dataContext;
+                point2.setAll({
+                    longitude: data.geometry.coordinates[0],
+                    latitude: data.geometry.coordinates[1]
+                })
+            });
+
+            return am5.Bullet.new(root, {
+                sprite: circle
+            });
+        });
+
+        citySeries.data.setAll(cities);
+
+        // Prepare line series data
+        let destinations = ["frankfurt", "hongkong"]
+        let mongoliaDataItem = citySeries.getDataItemById("mongolia");
+
+        // this will do all the animations
+        am5.array.each(destinations, function (did) {
+            let destinationDataItem = citySeries.getDataItemById(did);
+            let lineDataItem = lineSeries.pushDataItem({});
+            lineDataItem.set("pointsToConnect", [mongoliaDataItem, destinationDataItem])
+        });
+
+        let planeDataItem = planeSeries.pushDataItem({
+            lineDataItem,
+            positionOnLine: 0,
+            autoRotate: true
+        });
+
+        planeDataItem.dataContext = {};
+        resetPlaneAnimation()
+        // planeDataItem.animate({
+        //     key: "positionOnLine",
+        //     to: 1,
+        //     duration: 30000,
+        //     loops: Infinity,
+        //     easing: am5.ease.yoyo(am5.ease.linear)
+        // });
+
+        // planeDataItem.on("positionOnLine", (value) => {
+        //     // console.log(data.id, 'value', value)
+        //     if (planeDataItem.dataContext.prevPosition < value) {
+        //         plane.set("rotation", 0);
+        //     }
+
+        //     if (planeDataItem.dataContext.prevPosition > value) {
+        //         plane.set("rotation", -180);
+        //     }
+        //     planeDataItem.dataContext.prevPosition = value;
+        // });
+
+        function resetPlaneAnimation() {
+            // Re-animate the plane along the line
+            planeDataItem.animate({
+                key: "positionOnLine",
+                to: 1,
+                duration: 30000, // Adjust duration as needed
+                loops: Infinity, // Make sure it loops if desired
+                easing: am5.ease.yoyo(am5.ease.linear) // Smooth yoyo animation
+            });
+
+            // Update the rotation based on position
+            planeDataItem.on("positionOnLine", (value) => {
+                console.log(value);
+                if (planeDataItem.dataContext.prevPosition < value) {
+                    plane.set("rotation", 0);
+                }
+
+                if (planeDataItem.dataContext.prevPosition > value) {
+                    plane.set("rotation", -180);
+                }
+                planeDataItem.dataContext.prevPosition = value;
+            });
+        }
+
+        function addCity(coords, title) {
+            return pointSeries.pushDataItem({
+                latitude: coords.latitude,
+                longitude: coords.longitude
+            });
+        }
 
         return () => {
             root.dispose();
